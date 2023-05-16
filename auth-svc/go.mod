e6cb59d5819490f1527aa44e0064510ac93fc9de
Amal <amalmadhu06@gmail.com>
2023-05-16 18:57:11 +0530

    fix(build): fix loading env in auth service

--- auth-svc/go.mod
+++ auth-svc/go.mod
@@ -3,9 +3,10 @@ module github.com/amalmadhu06/go-grpc-microservices/auth-svc
 go 1.19
 
 require (
+	github.com/go-playground/validator/v10 v10.13.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/spf13/viper v1.15.0
-	golang.org/x/crypto v0.6.0
+	golang.org/x/crypto v0.7.0
 	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 	gorm.io/driver/postgres v1.5.0
@@ -14,6 +15,8 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -21,6 +24,7 @@ require (
 	github.com/jackc/pgx/v5 v5.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
--- auth-svc/pkg/config/config.go
+++ auth-svc/pkg/config/config.go
@@ -1,24 +1,43 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	Port         string `mapstructure:"PORT"`
-	DBUrl        string `mapstructure:"DB_URL"`
-	JWTSecretKey string `mapstructure:"JWT_SECRET_KEY"`
+	Port         string `mapstructure:"PORT"`           // Port represents the application port.
+	DBUrl        string `mapstructure:"DB_URL"`         // DBUrl represents URL for connecting with database
+	JWTSecretKey string `mapstructure:"JWT_SECRET_KEY"` // JWTSecretKey represents the secret used to sign JSON Web Token used for authentication
 }
 
+var envs = []string{
+	"PORT",
+	"DB_URL",
+	"JWT_SECRET_KEY",
+}
+
+// LoadConfig loads the application configuration from environment variables or a configuration file.
 func LoadConfig() (Config, error) {
 	var config Config
-	viper.AddConfigPath("./pkg/config/envs")
-	viper.SetConfigName("dev")
-	viper.SetConfigType("env")
 
-	viper.AutomaticEnv()
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
 
-	if err := viper.ReadInConfig(); err != nil {
-		return Config{}, err
+	for _, env := range envs {
+		if err := viper.BindEnv(env); err != nil {
+			return config, err
+		}
 	}
-	err := viper.Unmarshal(&config)
-	return config, err
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	if err := validator.New().Struct(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
