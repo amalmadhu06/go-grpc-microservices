1abe359d855626b51b9cc11af3f7e28e6083ec96
Amal <amalmadhu06@gmail.com>
2023-05-16 18:56:00 +0530

    fix(build): fix loading env in api-gateway

--- api-gateway/pkg/config/config.go
+++ api-gateway/pkg/config/config.go
@@ -1,42 +1,47 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
 
 // Config represents the application configuration.
 // It holds various settings that can be loaded from environment variables or a configuration file.
 type Config struct {
-	Port          string `mapstructure:"PORT"`
-	AuthSuvUrl    string `mapstructure:"AUTH_SUV_URL"`
-	ProductSuvUrl string `mapstructure:"PRODUCT_SUV_URL"`
-	OrderSuvUrl   string `mapstructure:"ORDER_SUV_URL"`
+	Port          string `mapstructure:"PORT"`            // Port represents the application port.
+	AuthSuvUrl    string `mapstructure:"AUTH_SUV_URL"`    // AuthSuvUrl represents the URL for the authentication service.
+	ProductSuvUrl string `mapstructure:"PRODUCT_SUV_URL"` // ProductSuvUrl represents the URL for the product service.
+	OrderSuvUrl   string `mapstructure:"ORDER_SUV_URL"`   // OrderSuvUrl represents the URL for the order service.
 }
 
-// LoadConfig loads the configuration settings.
-// It reads the configuration file, sets up environment variable support, and unmarshal the settings into a Config struct.
-// It returns the loaded Config and any error encountered during the process.
-func LoadConfig() (c Config, err error) {
+var envs = []string{
+	"PORT",
+	"AUTH_SUV_URL",
+	"PRODUCT_SUV_URL",
+	"ORDER_SUV_URL",
+}
 
-	// AddConfigPath adds the directory where the configuration file is located.
-	viper.AddConfigPath("./pkg/config/envs")
+// LoadConfig loads the application configuration from environment variables or a configuration file.
+func LoadConfig() (Config, error) {
+	var config Config
 
-	// SetConfigName sets the name of the configuration file to be read.
-	viper.SetConfigName("dev")
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
 
-	// SetConfigType sets the type of the configuration file.
-	viper.SetConfigType("env")
-
-	// AutomaticEnv enables automatic binding of environment variables to configuration values.
-	viper.AutomaticEnv()
-
-	// ReadInConfig reads the configuration file with the specified name and type.
-	err = viper.ReadInConfig()
-
-	// Check if there was an error reading the configuration file.
-	if err != nil {
-		return
+	for _, env := range envs {
+		if err := viper.BindEnv(env); err != nil {
+			return config, err
+		}
 	}
 
-	// Unmarshal reads the configuration settings into the Config struct.
-	err = viper.Unmarshal(&c)
-	return
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	if err := validator.New().Struct(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
